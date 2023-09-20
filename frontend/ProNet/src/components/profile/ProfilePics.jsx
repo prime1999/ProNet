6c9d74082d582adce4f4e29faf73ea8f61ce8eff
prime <primetechnologies02@gmail.com>
2023-09-20 21:02:21 +0100

    created the update profile pic and background photo functionalities

--- frontend/ProNet/src/components/profile/ProfilePics.jsx
+++ frontend/ProNet/src/components/profile/ProfilePics.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import axios from "axios";
 import { FiEdit2 } from "react-icons/fi";
 import { Avatar } from "@mui/material";
@@ -19,8 +19,14 @@ const ProfilePics = ({
 	const [alertMessage, setAlertMessage] = useState("");
 	const [alertSeverity, setAlertSeverity] = useState("success");
 
-	const [pic, setPic] = useState(intro?.pic);
+	const [pic, setPic] = useState("");
+	const [bgPic, setBgPic] = useState(null);
 	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setPic(intro?.pic);
+		setBgPic(intro?.backgroundPhoto);
+	}, [intro]);
 	// function to upload the user's profile picture
 	const handlePic = async (selectedPic) => {
 		try {
@@ -46,22 +52,82 @@ const ProfilePics = ({
 					"https://api.cloudinary.com/v1_1/ddboi173o/image/upload",
 					picData
 				);
-				setPic(data.url);
+				const picUrl = data.url;
+				setPic(picUrl);
 				setLoading(false);
 				const introUpdates = {
 					pic: data.url,
 				};
 				// dispatch the update profile intro function in the intro slice
 				dispatch(updateProfileIntro(introUpdates));
-				console.log(pic);
 				setFetchProfileIntroAgain(!fetchProfileIntroAgain);
+				// show success message
+				handleShowSnackbar("success", "Pic uploaded successfully");
 			} else {
 				setLoading(false);
+				// show error message
 				handleShowSnackbar("error", "Pic has to be a jpeg/png file");
 			}
 		} catch (error) {
 			setLoading(false);
+			// show error message
+			handleShowSnackbar("error", "could not upload pic");
+		}
+	};
+
+	// function to update the background photo
+	const handleBackgroundPhoto = async (selectedPic) => {
+		console.log(222);
+		try {
+			setLoading(true);
+
+			if (!selectedPic) {
+				setLoading(false);
+				return;
+			}
+			console.log(11);
+			// check if the image selected is either a jpeg or a png file
+			if (
+				selectedPic.type === "image/jpeg" ||
+				selectedPic.type === "image/png"
+			) {
+				console.log(22);
+				// create a new form data instance to send to cloudinary
+				const picData = new FormData();
+				// append the following key-value pairs to it
+				picData.append("file", selectedPic);
+				picData.append("upload_preset", "proNet");
+				picData.append("cloud_name", "ddboi173o");
+				console.log(33);
+				// send the data(your new FormData with the required data) to your cloudinary url
+				const { data } = await axios.post(
+					"https://api.cloudinary.com/v1_1/ddboi173o/image/upload",
+					picData
+				);
+				console.log(44);
+				const picUrl = data.url;
+				setBgPic(picUrl);
+				setLoading(false);
+				const introUpdates = {
+					backgroundPhoto: data.url,
+				};
+				// dispatch the update profile intro function in the intro slice
+				dispatch(updateProfileIntro(introUpdates));
+				setFetchProfileIntroAgain(!fetchProfileIntroAgain);
+				// show success message
+				handleShowSnackbar("success", "background photo uploaded successfully");
+			} else {
+				setLoading(false);
+				// show error message
+				handleShowSnackbar(
+					"error",
+					"background photo has to be a jpeg/png file"
+				);
+			}
+		} catch (error) {
 			console.log(error);
+			setLoading(false);
+			// show error message
 			handleShowSnackbar("error", "could not upload pic");
 		}
 	};
@@ -101,8 +167,8 @@ const ProfilePics = ({
 					/>
 				</div>
 			</div>
-			{/* <div className="">
-				<label htmlFor="profilePic">
+			<div className="">
+				<label htmlFor="profileBgPic">
 					<p className="bg-light p-2 rounded-full duration-500 hover:cursor-pointer hover:bg-orange">
 						<FiEdit2 />
 					</p>
@@ -110,11 +176,11 @@ const ProfilePics = ({
 				<input
 					type="file"
 					accept="image/*"
-					id="profilePic"
+					id="profileBgPic"
 					className="hidden"
-					onChange={(e) => handlePic(e.target.files[0])}
+					onChange={(e) => handleBackgroundPhoto(e.target.files[0])}
 				/>
-			</div> */}
+			</div>
 			<NotificationAlert
 				open={openAlert}
 				message={alertMessage}
