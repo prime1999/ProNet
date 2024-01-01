df4e997e880be290e8b3d5fc31d66d1837585fc0
prime <primetechnologies02@gmail.com>
2024-01-01 06:32:10 +0000

    testing

--- frontend/ProNet/src/pages/LandingPage/Home.jsx
+++ frontend/ProNet/src/pages/LandingPage/Home.jsx
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import NavBar from "../../Layouts/LandingPage/NavBar";
-import HeroSection from "../../Layouts/LandingPage/HeroSection";
-import FirstSection from "../../Layouts/LandingPage/FirstSection";
-import SecondSection from "../../Layouts/LandingPage/SecondSection";
-import ThirdSection from "../../Layouts/LandingPage/ThirdSection";
-import FourthSection from "../../Layouts/LandingPage/FourthSection";
-import FifthSection from "../../Layouts/LandingPage/FIfthSection";
-import Footer from "../../Layouts/Footer";
+// import NavBar from "../../Layouts/LandingPage/NavBar";
+// import HeroSection from "../../Layouts/LandingPage/HeroSection";
+// import FirstSection from "../../Layouts/LandingPage/FirstSection";
+// import SecondSection from "../../Layouts/LandingPage/SecondSection";
+// import ThirdSection from "../../Layouts/LandingPage/ThirdSection";
+// import FourthSection from "../../Layouts/LandingPage/FourthSection";
+// import FifthSection from "../../Layouts/LandingPage/FIfthSection";
+// import Footer from "../../Layouts/Footer";
 
 const Home = () => {
 	const navigate = useNavigate();
