c1f49d27701b3ec07b4569ba6d41ab865430dff3
prime <primetechnologies02@gmail.com>
2024-01-01 06:40:26 +0000

    added a dependency

--- frontend/ProNet/src/pages/LandingPage/Home.jsx
+++ frontend/ProNet/src/pages/LandingPage/Home.jsx
@@ -1,15 +1,8 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-// import NavBar from "../../Layouts/LandingPage/NavBar";
-// import HeroSection from "../../Layouts/LandingPage/HeroSection";
-// import FirstSection from "../../Layouts/LandingPage/FirstSection";
-// import SecondSection from "../../Layouts/LandingPage/SecondSection";
-// import ThirdSection from "../../Layouts/LandingPage/ThirdSection";
-// import FourthSection from "../../Layouts/LandingPage/FourthSection";
-// import FifthSection from "../../Layouts/LandingPage/FIfthSection";
-// import Footer from "../../Layouts/Footer";
-
+import NavBar from "../../Layouts/LandingPage/NavBar";
+import FirstSection from "../../Layouts/LandingPage/FirstSection";
 const Home = () => {
 	const navigate = useNavigate();
 	const { user } = useSelector((state) => state.auth);
@@ -21,7 +14,20 @@ const Home = () => {
 	}, []);
 	return (
 		<>
-			<h1>test</h1>
+			<div>
+				<div className="container w-10/12 mx-auto">
+					<NavBar />
+					<HeroSection />
+					<FirstSection />
+					<SecondSection />
+				</div>
+				<ThirdSection />
+				<div className="container w-10/12 mx-auto">
+					<FourthSection />
+					<FifthSection />
+				</div>
+				<Footer />
+			</div>
 		</>
 	);
 };
