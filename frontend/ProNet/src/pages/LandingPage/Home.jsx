c66b0b9aa778476e8bb11919eb1c6ad6935ad98c
prime <primetechnologies02@gmail.com>
2024-01-01 06:41:35 +0000

    made deployment changes

--- frontend/ProNet/src/pages/LandingPage/Home.jsx
+++ frontend/ProNet/src/pages/LandingPage/Home.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import NavBar from "../../Layouts/LandingPage/NavBar";
-import FirstSection from "../../Layouts/LandingPage/FirstSection";
+
 const Home = () => {
 	const navigate = useNavigate();
 	const { user } = useSelector((state) => state.auth);
@@ -14,20 +13,7 @@ const Home = () => {
 	}, []);
 	return (
 		<>
-			<div>
-				<div className="container w-10/12 mx-auto">
-					<NavBar />
-					<HeroSection />
-					<FirstSection />
-					<SecondSection />
-				</div>
-				<ThirdSection />
-				<div className="container w-10/12 mx-auto">
-					<FourthSection />
-					<FifthSection />
-				</div>
-				<Footer />
-			</div>
+			<h1>test</h1>
 		</>
 	);
 };
